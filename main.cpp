027c47453d38d7c4084a8a3d8693a64309ff7e30
Stephen Comarata <comarasj@mail.uc.edu>
2021-02-03 20:40:19 -0500

    adding basic files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/** EECE 5183 - 001 Compiler Theory 
+ * Stephen Comarata
+ * main.cpp
+ * 
+ * This program is the driver program for the compiler project 
+ * 
+ * 
+ */
+
+
+//Includes
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
